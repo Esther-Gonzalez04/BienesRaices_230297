f24b75d1e47ecbb9f56e3897527ed80c44c0bddc
Esther González Peralta <230297@utxicotepec.edu.mx>
2024-11-10 15:59:56 -0600

    Practica12
    Se agregaron colores a tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ['./views/**/*.pug'],
   theme: {
     extend: {},
+    colors:{
+      'p_blue':{
+        100: '#B1EDE8',
+        200: '#0D1B2A',
+      },
+      'p_yellow': '#E7E247',
+      'black': '#000',
+      'white': '#FFF',
+      'gray':{
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280'
+      },
+      'shadow': 'shadow'
+    },
   },
   plugins: [],
 }
